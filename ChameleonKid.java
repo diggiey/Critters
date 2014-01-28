0f8988afe34e2d351287029bced09e55dac45ab9
apcs <apcs@mvhs-u52810.dcsdk12.org>
2014-01-28 09:09:31 -0700

    Didn't get to finish

---
+++ ChameleonKid.java
@@ -0,0 +1,36 @@
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.Critter;
+import info.gridworld.grid.Location;
+import java.awt.Color;
+
+import java.util.ArrayList;
+
+public class ChameleonKid extends Critter
+{
+
+    private static final double DARKENING_FACTOR = 0.05;
+
+    public void processActors(ArrayList<Actor> actors)
+    {
+        int n = actors.size();
+        if (n == 0) {
+            Color c = getColor();
+            int red = (int) (c.getRed() * (1 - DARKENING_FACTOR));
+            int green = (int) (c.getGreen() * (1 - DARKENING_FACTOR));
+            int blue = (int) (c.getBlue() * (1 - DARKENING_FACTOR));
+            setColor(new Color(red, green, blue));
+            return;
+        }
+        int r = (int) (Math.random() * n);
+
+        Actor other = actors.get(r);
+        System.out.println(other);
+        setColor(other.getColor());
+    }
+
+    public void makeMove(Location loc)
+    {
+        setDirection(getLocation().getDirectionToward(loc));
+        super.makeMove(loc);
+    }
+}
