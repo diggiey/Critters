fe0c24a4961b6d552b0f00d0800d42bf465616b0
apcs <apcs@mvhs-u52810.dcsdk12.org>
2014-02-11 08:25:33 -0700

    Finished BlusterCritter

--- BlusterCritter.java
+++ BlusterCritter.java
@@ -6,9 +6,11 @@ import java.awt.Color;
 import java.util.ArrayList;
 
 public class BlusterCritter extends Critter {
+
+	private int c;
 	
 	public BlusterCritter(int c) {
-
+		this.c = c;
 	}
 
 	public ArrayList<Actor> getActors()
@@ -24,38 +26,35 @@ public class BlusterCritter extends Critter {
 
 	    getGrid().getValidAdjacentLocations(loc);
 
-	    for (int r = loc.getRow() - 2; r<=2; r++) {
-	    	for (int c = loc.getCol() - 2; c <= 2; c++) {
-	    		if (!(a instanceof Critter)) {
-	    		    
-	    		} else {
-	    			amountOfCritters++;
+	    for (int r = loc.getRow() - 2; r<= loc.getRow()+2; r++) {
+	    	for (int z = loc.getCol() - 2; z <= loc.getCol()+2; z++) {
+	    		Location a = new Location(r, z);
+
+	    		if (getGrid().isValid(a)) {
+	    			if ((getGrid().get(a)) instanceof Critter) {
+	    				amountOfCritters++;
+	    			}
 	    		}
 	    	}
 	    }
 
-	    // for ( Location l : getGrid().getValidAdjacentLocations(loc)) {
-	    	
-	    // }
+	    System.out.println(amountOfCritters);
 
-		int emptyLocations = getGrid().getEmptyAdjacentLocations(getLocation()).size();
-		int c = getActors().size();
-
-		if (emptyLocations > c) {
+		if (amountOfCritters - 1 < this.c) {
 
 			Color color = getColor();
 
-			int red = (int) (color.getRed() + (1));
+			int red = (int) (color.getRed() + (10));
 			if (red >= 255) {
 				red = 255;
 			}
 
-			int green = (int) (color.getGreen() + (1));
+			int green = (int) (color.getGreen() + (10));
 			if (green >= 255) {
 				green = 255;
 			}
 
-			int blue = (int) (color.getBlue() + (1));
+			int blue = (int) (color.getBlue() + (10));
 			if (blue >= 255) {
 				blue = 255;
 			}
@@ -66,17 +65,17 @@ public class BlusterCritter extends Critter {
 
 			Color color = getColor();
 
-			int red = (int) (color.getRed() - (1));
+			int red = (int) (color.getRed() - (10));
 			if (red <= 0) {
 				red = 0;
 			}
 
-			int green = (int) (color.getGreen() - (1));
+			int green = (int) (color.getGreen() - (10));
 			if (green <= 0) {
 				green = 0;
 			}
 
-			int blue = (int) (color.getBlue() - (1));
+			int blue = (int) (color.getBlue() - (10));
 			if (blue <= 0) {
 				blue = 0;
 			}
