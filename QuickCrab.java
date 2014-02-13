6fb58f99c487f790e16283d293dcc4becc3444a0
apcs <apcs@mvhs-u52810.dcsdk12.org>
2014-02-13 09:33:26 -0700

    Finished

--- QuickCrab.java
+++ QuickCrab.java
@@ -1,49 +1,26 @@
 import info.gridworld.actor.Actor;
 import info.gridworld.actor.Critter;
-import info.gridworld.actor.Rock;
-import info.gridworld.grid.*;
+import info.gridworld.grid.Grid;
+import info.gridworld.grid.Location;
+
 import java.awt.Color;
 import java.util.ArrayList;
 
-public class QuickCrab extends CrabCritter{
+public class QuickCrab extends CrabCritter {
 	public QuickCrab() {
-
+		
 	}
-
-	public ArrayList<Location> getMoveLocations()
-	{
+	public ArrayList<Location> getLocationsInDirections(int[] directions) {
 	    ArrayList<Location> locs = new ArrayList<Location>();
-	    int[] dirs =
-	        { Location.LEFT, Location.RIGHT };
-	    for (Location loc : getLocationsInDirections(dirs)) {
+	 	Grid gr = getGrid();
+	    Location loc = getLocation();
 
-	    	int direction = getDirection();
-
-	    	int r = loc.getRow();
-	    	int c = loc.getCol();
-	    	
-	    	if (direction == 0 || direction == 180) {
-	    		if (c > getLocation().getCol()) {
-	    			c++;
-	    		} else {
-	    			c--;
-	    		}
-	    	} else {
-	    		if (r > getLocation().getCol()) {
-	    			r++;
-	    		} else {
-	    			r--;
-	    		}
-	    	}
-
-	    	ArrayList<Location> locs2 = new ArrayList<Location>();
-	    	
-
-	        if (getGrid().get(loc2) == null) {
-	            locs.add(loc2);
-	        }
+	    for (int d : directions) {
+	        Location neighborLoc = loc.getAdjacentLocation(getDirection() + d);
+	        Location secondLoc = neighborLoc.getAdjacentLocation(getDirection() + d);
+	        if (gr.isValid(neighborLoc) && gr.isValid(secondLoc) && gr.get(neighborLoc) == null && gr.get(secondLoc) == null)
+	            locs.add(secondLoc);
 	    }
-
 	    return locs;
-	}
+	}    
 }
